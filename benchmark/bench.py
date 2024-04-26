36795a42db0a9401f8eed878a2c793c0b0fd0e1a
ryoh <lucrod1000@gmail.com>
2024-04-26 02:37:41 +0200

    try to generate csv

--- benchmark/bench.py
+++ benchmark/bench.py
@@ -6,98 +6,125 @@ import psutil
 import time
 
 nth_iterations = 2
-max_threads = 16
-og_dir = os.getcwd()
+max_threads = 4
 output_index_time = 2
-
 num_processors = os.cpu_count()
-if max_threads > num_processors:
-    max_threads = num_processors
+
 
 programs_names = [
     ("LIFO scheduler","test_lifo_quicksort"),
-    ("LIFO scheduler with semaphore and synchronisation variable", "test_lifo_quicksort_sem"),
+    ("LIFO scheduler with semaphore and synchronization variable", "test_lifo_quicksort_sem"),
     ("Work-stealing scheduler", "test_stealing_quicksort"),
     ("Work-stealing scheduler with condition variable", "test_stealing_quicksort_cond"),
-    ("Work-stealing scheduler with semaphore and synchronisation variable", "test_stealing_quicksort_sem")
+    ("Work-stealing scheduler with semaphore and synchronization variable", "test_stealing_quicksort_sem")
 ]
 
-os.chdir("..")
+results = [[[None for _ in range(nth_iterations)] for _ in range(max_threads + 1)] for _ in range(len(programs_names))]
+results_avg = [[None] * (max_threads + 1) for _ in range(len(programs_names))]
 
-print("Compiling files...")
 
-subprocess.run(["/usr/bin/make", "clean"], stdout=subprocess.DEVNULL)
+def compile_files():
+    og_dir = os.getcwd()
+    os.chdir("..")
+    print("Compiling files...")
+    subprocess.run(["/usr/bin/make", "clean"], stdout=subprocess.DEVNULL)
+    cmd = ["/usr/bin/make"] + [program[1] for program in programs_names]
+    subprocess.run(cmd, stdout=subprocess.DEVNULL)
+    os.chdir(og_dir)
 
-cmd = ["/usr/bin/make"] + [program[1] for program in programs_names]
-
-subprocess.run(cmd, stdout=subprocess.DEVNULL)
-
-os.chdir(og_dir)
-
-results = [[None] * (max_threads + 1) for _ in range(len(programs_names))]
 
 def launch_prog(pair, args):
     cmd = "../out/" + pair[1]
     result = subprocess.run([cmd, args], stdout=subprocess.PIPE)
     s = result.stdout.decode().split()
     res = float(s[output_index_time])
-    f.write(result.stdout.decode())
+    # f.write(result.stdout.decode())
     return res
 
 
-with open("results.txt", "w") as f:
+def launch_bench():
     print(f"Executing benchmark with {nth_iterations} iterations, from 1 to {max_threads} threads")
     for count, pair in enumerate(programs_names):
-        f.write(pair[0] + "\n")
+        # f.write(pair[0] + "\n")
 
         # Launch serial reference
-        f.write("Serial time reference:\n")
+        # f.write("Serial time reference:\n")
         args = "-s"
         serial_res = launch_prog(pair, args)
-        results[count][0] = serial_res
+        results_avg[count][0] = serial_res
+        results[count][0][0] = serial_res
 
         for i in range(1, max_threads + 1):
             program_bench_sum = 0.
-            for _ in range(nth_iterations):
+            for j in range(nth_iterations):
                 # Launch program with each threads from 1 to max_threads
                 args = "-t " + str(i)
                 res = launch_prog(pair, args)
                 program_bench_sum += res
+                results[count][i][j] = res
                 time.sleep(0.3)
-
             average_time = round(program_bench_sum / nth_iterations, 6)
-            results[count][i] = average_time
-            f.write("Average time with " + str(i) + " threads: " + str(average_time) + "\n")
-        f.write("\n")
+            results_avg[count][i] = average_time
+            # f.write("Average time with " + str(i) + " threads: " + str(average_time) + "\n")
+        # f.write("\n")
 
-
-import matplotlib.pyplot as plt
-import numpy as np
-import matplotlib as mpl
 
 min_x_axis = 0
 max_x_axis = max_threads + 1
 min_y_axis = 0
 max_y_axis = 1.5
 
-print(results)
 
-for i, values in enumerate(results):
-    program_name = programs_names[i][1].replace(" ", "_")
+def generate_images():
+    import matplotlib.pyplot as plt
+    import numpy as np
 
-    plt.plot(values)
-    plt.xlabel('Threads')
-    plt.ylabel('Time in seconds')
-    plt.title(programs_names[i][0] + "\n(average at " + str(nth_iterations) + " iterations)")
+    for i, values in enumerate(results_avg):
+        program_name = programs_names[i][1].replace(" ", "_")
 
-    plt.ylim(min_y_axis, max_y_axis)
-    plt.xlim(min_x_axis, max_x_axis)
+        plt.plot(values)
+        plt.xlabel('Threads')
+        plt.ylabel('Time in seconds')
+        plt.title(programs_names[i][0] + "\n(average at " + str(nth_iterations) + " iterations)")
 
-    # Replace the 0 tick label with "(serial)"
-    x_values = np.arange(min_x_axis, max_x_axis)  # Adjusted to start from 0
-    tick_labels = ['(serial)' if x == 0 else str(x) for x in x_values]
-    plt.xticks(x_values, tick_labels)
+        plt.ylim(min_y_axis, max_y_axis)
+        plt.xlim(min_x_axis, max_x_axis)
 
-    # Save the plot to a file (e.g., PNG, PDF, SVG)
-    plt.savefig(f'{program_name}_plot.png')
-    plt.close()
+        # Replace the 0 tick label with "(serial)"
+        x_values = np.arange(min_x_axis, max_x_axis)  # Adjusted to start from 0
+        tick_labels = ['(serial)' if x == 0 else str(x) for x in x_values]
+        plt.xticks(x_values, tick_labels)
+
+        # Save the plot to a file (e.g., PNG, PDF, SVG)
+        plt.savefig(f'{program_name}_plot.png')
+        plt.close()
+
+
+output_csv_filename = "benchmark_results.csv"
+
+
+def generate_csv_data():
+    import csv
+
+    for i, sub_array in enumerate(results):
+        name = programs_names[i][0].replace(' ', '_').replace('-', '_')
+        with open(f'bench_{name}.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([programs_names[i][0], ""])
+            writer.writerow(["Threads", "Time in seconds", ""])
+            writer.writerow(["", ""])
+            for j in range(1, max_threads + 1):
+                if j-1 < len(sub_array):
+                    writer.writerow([j] + sub_array)
+                else:
+                    writer.writerow([j, ""])
+
+
+if __name__ == "__main__":
+    if max_threads > num_processors:
+        max_threads = num_processors
+    compile_files()
+    launch_bench()
+    print(results)
+    generate_images()
+    generate_csv_data()
